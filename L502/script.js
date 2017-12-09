fab2bb9df45d550cfbc30993a3242208eebbb399
tornike001 <33287858+tornike001@users.noreply.github.com>
2017-12-09 20:45:22 +0400

    L502
    L502

---
+++ L502/script.js
@@ -0,0 +1,5 @@
+var users = document.getElementById('users');
+var empty = document.getElementById('empty-list');
+
+alert("users ცარიელია: " + (users.children.length == 0));
+alert("empty-list ცარიელია: " + (empty.children.length == 0));
